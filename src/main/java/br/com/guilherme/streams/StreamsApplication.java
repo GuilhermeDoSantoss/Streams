9652b1dba2539f4ba4d01db658e1aba2923c616d
Guilherme dos Santos <guilherme1996santos@icloud.com>
2025-05-02 16:58:04 -0300

    Salvando alterações locais antes do rebase

--- src/main/java/br/com/guilherme/streams/StreamsApplication.java
+++ src/main/java/br/com/guilherme/streams/StreamsApplication.java
@@ -1,13 +1,29 @@
 package br.com.guilherme.streams;
 
-import org.springframework.boot.SpringApplication;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+import java.lang.reflect.Type;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 
 @SpringBootApplication
 public class StreamsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StreamsApplication.class, args);
+		try{
+			String jsonStr = new String (Files.readAllBytes(Paths.get("Pessoa.json")));
+			Gson gson = new Gson();
+			Type type = new TypeToken<List<Pessoa>>() {}.getType();
+			List<Pessoa> lista = gson.fromJson(jsonStr, type);
+
+			lista.forEach(p->System.out.println(p));
+		} catch(Exception ex){
+			ex.printStackTrace();
+		}
 	}
 
 }
